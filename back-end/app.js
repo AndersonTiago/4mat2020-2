ce24beac569f76a709758059f848e3602efbbd2b
AndersonTiago <andersonbonamim@gmail.com>
2020-08-26 14:09:01 +0000

    (26/08) instalação do mongoose para conexão com MongoDb

--- back-end/app.js
+++ back-end/app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+const db = require('./config/database');
+//db(mongodb+srv://atbcosta:<password>@cluster0.hmiue.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority);
+
+
 var app = express();
 
 app.use(logger('dev'));
