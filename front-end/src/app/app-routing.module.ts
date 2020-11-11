f08a043858ff05672025e6bf14bb54fff2cc1da5
AndersonTiago <andersonbonamim@gmail.com>
2020-11-11 14:07:09 +0000

    (11/11) Término do componente de listagem e início de componente de formulário

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,14 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {​​ NgModule }​​ from '@angular/core';
+
+import {​​ Routes, RouterModule }​​ from '@angular/router';
+
 import {​​ CursoListComponent }​​ from './curso/curso-list/curso-list.component';
 
+import {​​ CursoFormComponent }​​ from './curso/curso-form/curso-form.component';
+
+
+
 const routes: Routes = [
-    // Rotas no Angular NUNCA começam com /
-    {path: 'curso', component: CursoListComponent}
+
+    // Rotas no Angular NUNCA começam com barra
+
+    {​​path: 'curso', component: CursoListComponent}​​,
+
+    {​​path: 'curso/novo', component: CursoFormComponent}​​,
+
+    {​​path: 'curso/:id', component: CursoFormComponent}​​
+
 ];
 
-@NgModule({
+
+
+@NgModule({​​
+
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+}​​)
+
+export class AppRoutingModule {​​ }​​
